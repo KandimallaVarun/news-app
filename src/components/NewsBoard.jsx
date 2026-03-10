c86739ec222aadd8de1a9b4391f416848043f124
KandimallaVarun <varunkandimalla333@gmail.com>
2026-03-11 00:58:17 +0530

    lastest 4

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -5,7 +5,7 @@ export const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=53c8497c29bf4dc383e1242339f673d1`;
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
     console.log("fetching", url);
     fetch(url)
       .then(r => r.json())
@@ -21,8 +21,7 @@ export const NewsBoard = ({ category }) => {
         console.error("Error fetching articles:", error);
         setArticles([]);
       });
-
-      console.log(url);
+      console.log("url :", url);
   }, [category]);
   
   return (
